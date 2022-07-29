986d6177f088ae16fd50bd3be7afeb1671921e3d
chutki12 <priyankachaudhary2068>
2022-07-29 17:43:56 +0530

    ADDED : Welcome To Stock Account Management System

---
+++ StockAccountMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.stockAccount;
+
+public class StockAccountMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome To Stock Account Management System");
+		
+
+	}
+
+}
