cf4eb7533e6a06d5786e03b472066628fa220d00
chutki12 <priyankachaudhary2068>
2022-07-29 18:56:14 +0530

    ADDED : Readind the stock data, calculating and display the stock report

---
+++ Stock.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.stockAccount;
+
+public class Stock {
+	
+	private String stockName;
+    private int numberOfShare;
+    private double sharePrice;
+
+    Stock(String stockName, int numberOfShare, double sharePrice) {
+
+        this.stockName = stockName;
+        this.numberOfShare = numberOfShare;
+        this.sharePrice = sharePrice;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public int getNumberOfShare() {
+        return numberOfShare;
+    }
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+}
+
+
+
+
+
